84a50b0ea07daea7e882fb90c0cf4b1000947ccf
Mohamed LAHRECH <lahrechm@intellisolutions.com>
2023-02-07 11:43:34 +0100

    change pic

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from PIL import Image
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "resume.pdf"
-profile_pic = current_dir / "assets" / "pic.PNG"
+profile_pic = current_dir / "assets" / "pic.png"
 
 #------------GENERAL SETTINGS-----------------
 
